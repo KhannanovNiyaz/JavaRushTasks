1422cab14d8ed6292cd05771b13c072ca412c70b
KhannanovNiyaz <gut_boy@mail.ru>
2019-02-09 22:20:10 +0300

    Tasks 3707 has done

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -1,15 +1,55 @@
 package com.javarush.task.task20.task2025;
 
-/* 
+import java.util.Arrays;
+
+/*
 Алгоритмы-числа
 */
 public class Solution {
     public static long[] getNumbers(long N) {
-        long[] result = null;
+        String number = String.valueOf(N);
+        long[] result = new long[number.length()];
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < number.length(); i++) {
+            int z = Character.getNumericValue(number.charAt(i));
+//            buffer.append(Math.pow(z, number.length()));
+            buffer.append((int)Math.pow(z, number.length()) + ",");
+        }
+
+//        System.out.println(buffer.toString());
+
+        String[] res = buffer.toString().split(",");
+        int sum = 0;
+        for (int i = 0; i < res.length; i++) {
+//            sum += Long.parseLong(res[i]);
+            sum += Integer.parseInt(res[i]);
+        }
+//        System.out.println(sum);
+
+        String[] split = String.valueOf(sum).split("");
+
+//        String[] s = new StringBuffer(sum).toString().split("");
+
+
+        for (int i = 0; i < split.length; i++) {
+            result[i] = Integer.parseInt(split[i]);
+        }
         return result;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(getNumbers(370)));
+        System.out.println(Arrays.toString(getNumbers(8208)));
     }
 }
+/*
+Число S состоит из M цифр, например, S=370 и M (количество цифр) = 3
+Реализовать логику метода getNumbers, который должен среди натуральных чисел меньше N (long)
+находить все числа, удовлетворяющие следующему критерию:
+число S равно сумме его цифр, возведенных в M степень
+getNumbers должен возвращать все такие числа в порядке возрастания.
+
+Пример искомого числа:
+370 = 3*3*3 + 7*7*7 + 0*0*0
+8208 = 8*8*8*8 + 2*2*2*2 + 0*0*0*0 + 8*8*8*8
+* */
--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,10 +1,14 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
+import java.util.function.Predicate;
 
 public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable, Set {
-    public static final Object PRESENT = new Object();
+    private static final Object PRESENT = new Object();
     private transient HashMap<E, Object> map;
 
     public AmigoSet() {
@@ -12,16 +16,97 @@ public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable,
     }
 
     public AmigoSet(Collection<? extends E> collection) {
-        map = new HashMap<>(12);
+        map = new HashMap<>(Math.max((int) (collection.size() / .75f) + 1, 16));
+        addAll(collection);
     }
 
     @Override
-    public Iterator iterator() {
-        return null;
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
+
+    @Override
+    public boolean add(Object o) {
+        return map.put((E) o, PRESENT) == null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return map.containsKey(o);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public Object clone() {
+
+        try {
+            AmigoSet<E> amigoSet = (AmigoSet<E>) super.clone();
+            amigoSet.map = (HashMap<E, Object>) map.clone();
+            return amigoSet;
+        } catch (Exception e) {
+            throw new InternalError();
+        }
+    }
+
+    private void writeObject(ObjectOutputStream outputStream) throws IOException {
+        outputStream.defaultWriteObject();
+        outputStream.writeInt(map.size());
+        outputStream.writeFloat(HashMapReflectionHelper.callHiddenMethod(map, "loadFactor"));
+        outputStream.writeInt(HashMapReflectionHelper.callHiddenMethod(map, "capacity"));
+        for (E e : map.keySet()) {
+            outputStream.writeObject(e);
+        }
+    }
+
+    private void readObject(ObjectInputStream inputStream) throws IOException, ClassNotFoundException {
+        inputStream.defaultReadObject();
+        int mapSize = inputStream.readInt();
+        float mapLoadFactor = inputStream.readFloat();
+        int mapCapacity = inputStream.readInt();
+
+        map = new HashMap<>(mapCapacity, mapLoadFactor);
+        for (int i = 0; i < mapSize; i++) {
+            map.put((E) inputStream.readObject(), PRESENT);
+        }
+
+    }
+
 }
+/*
+1. Реализуй свою логику сериализации и десериализации.
+Вспоминай, какие именно приватные методы нужно добавить, чтоб сериализация пошла по твоему сценарию.
+Для сериализации:
+* сериализуй сет
+* сериализуй capacity и loadFactor у объекта map, они понадобятся для десериализации.
+Т.к. эти данные ограничены пакетом, то воспользуйся утилитным классом HashMapReflectionHelper, чтобы достать их.
+Для десериализации:
+* вычитай все данные
+* создай мапу используя конструктор с capacity и loadFactor
+
+2. Помнишь, что такое transient?
+Требования:
+1. В классе AmigoSet должен содержаться private метод writeObject с одним параметром типа ObjectOutputStream.
+2. В классе AmigoSet должен содержаться private метод readObject с одним параметром типа ObjectInputStream.
+3. В методе writeObject должен быть вызван метод defaultWriteObject на объекте типа ObjectOutputStream полученном в качестве параметра.
+4. В методе readObject должен быть вызван метод defaultReadObject на объекте типа ObjectInputStream полученном в качестве параметра.
+5. Объект сериализованный с помощью метода writeObject должен быть равен объекту десериализованному с помощью метода readObject.
+* */
